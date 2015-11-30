c0cd38c8ceaef05bf88d24a6671c04629ce48fff
Greg Hale <imalsogreg@gmail.com>
2015-11-30 16:35:05 -0500

    initial commit

---
+++ js/retrospective.js
@@ -0,0 +1,13 @@
+var main = function(){
+  $('#test').innerHTML='HELLO';
+}
+
+$('document').ready( alert('we"re ready') );
+
+function clicked() {
+  alert('clicked');
+}
+
+function clicked2() {
+  alert('clicked2');
+}
