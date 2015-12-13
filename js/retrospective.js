86b72a4225f5b4849102f472b3ff15a196381c91
Hector <penagos@mit.edu>
2015-12-13 08:25:32 -0500

    Code is functional and includes server code

--- js/retrospective.js
+++ js/retrospective.js
@@ -3,14 +3,14 @@ var start_time = 0;                  /* Timestamp new trial events*/
 /* Keep track of selections and number of trials. Elements 0,2,4,6 track selections to the right-side options. Elements 1,3,5,7 track selections to the left-side options. Element 8 tracks overall number of trials*/
 var counter = [0,0,0,0,0,0,0,0,0];   /* Count selections and trials*/
 var Telapsed = [];                   /* Keep track of timing between trial start and selection*/
-var payout = [];                     /* Keep track of earnings throughout task*/
+var payoutsch = [0,0,0.10,0.25,0.25,1,5,10];    /* Payout schedule*/
 
 var active = true;                /* Control when clicking is available */
 
-var Trial_type1 = 1;
-var Trial_type2 = 1;
-var Trial_type3 = 2;
-var Trial_type4 = 3;
+var Trial_type1 = 5;
+var Trial_type2 = 5;
+var Trial_type3 = 5;
+var Trial_type4 = 5;
 
 var maxTrials = Trial_type1+Trial_type2+Trial_type3+Trial_type4;
 
@@ -19,104 +19,132 @@ function setUpTrial(rightid,leftid) {
     /* identify elements used to display choices */
     var rightdiv = document.getElementById(rightid);
     var leftdiv = document.getElementById(leftid);
-    var backdiv = document.getElementById("background");
+    var statediv = document.getElementById("statescreen");
+    var paydiv = document.getElementById("outcomescreen");
     var instdiv = document.getElementById("instruction");
-    var messp = document.getElementById("inst_message");
+
     var rightim = document.getElementById("right_img");
     var leftim = document.getElementById("left_img");
-    var payim = document.getElementById("PayorState");
+    var stateim = document.getElementById("State");
+    var payoutim = document.getElementById("Payout");
+    var messp = document.getElementById("inst_message");
 
     if (counter[8] < maxTrials) {
         /* format choice on right side*/
-        rightdiv.style.width = "750px";
-        rightdiv.style.height = "500px";
-        rightdiv.style.right = "25px";
-        rightdiv.style.top = "200px";
+        rightdiv.style.width = "40%";
+        rightdiv.style.height = "40%";
+        rightdiv.style.right = "5%";
+        rightdiv.style.top = "30%";
         rightdiv.style.display = "block";
         rightdiv.style.left = "";
         /* format choice on left side*/
-        leftdiv.style.width = "750px";
-        leftdiv.style.height = "500px";
-        leftdiv.style.left = "25px";
-        leftdiv.style.top = "200px";
+        leftdiv.style.width = "40%";
+        leftdiv.style.height = "40%";
+        leftdiv.style.left = "5%";
+        leftdiv.style.top = "30%";
         leftdiv.style.display = "block";
-        /* format background*/
-        backdiv.style.display = "block";
-        backdiv.style.width = "100%";
-        backdiv.style.height = "100%";
-        /* format instruction message*/
+        /* make state screen visible*/
+        statediv.style.display = "block";
+        statediv.style.width = "100%";
+        statediv.style.height = "70%";
+        statediv.style.top = "0%";
+        /* make payout screen not visible*/
+        paydiv.style.display = "none";
+        paydiv.style.width = "100%";
+        paydiv.style.height = "100%";
+        /* make instruction screen visible*/
         instdiv.style.display = "block";
         instdiv.style.width = "100%";
-        instdiv.style.height = "100%";
-        messp.style.display = "block";
-        /* reposition figures*/
-        leftim.style.marginLeft = "225px";
-        rightim.style.marginLeft = "225px";
+        instdiv.style.height = "70%";
+        instdiv.style.bottom = "0%";
+
+        /* format fractal images*/
+        leftim.style.width = "60%";
+        leftim.style.height = "60%";
+        leftim.style.left = "20%";
+        leftim.style.top = "15%";
+        leftim.style.display = "block";
+
+        rightim.style.width = "60%";
+        rightim.style.height = "60%";
+        rightim.style.left = "20%";
+        rightim.style.top = "15%";
+        rightim.style.display = "block";
 
         /* determine what to display depending in trial type*/
         if (counter[8] < Trial_type1) {
-            leftim.src = "../img/fractal1.jpg";
-            rightim.src = "../img/fractal2.png";
-
-            leftdiv.style.backgroundcolor = "#990000";
-            rightdiv.style.backgroudcolor = "#003300";
-
-            instdiv.style.backgroundColor = "black";
-            backdiv.style.backgroundColor = "black";
-        }
-        else if (counter[8] < Trial_type1+Trial_type2){
-            leftim.src = "../img/fractal1.jpg";
-            rightim.src = "../img/fractal2.png";
+            leftim.src = "../img/fractal1hr.jpg";
+            rightim.src = "../img/fractal2hr.jpg";
 
             leftdiv.style.backgroundColor = "#990000";
             rightdiv.style.backgroudColor = "#003300";
 
+            statediv.style.display = "none";
+            instdiv.style.height = "100%";
             instdiv.style.backgroundColor = "black";
-            backdiv.style.backgroundColor = "black";
+        }
+        else if (counter[8] < Trial_type1+Trial_type2){
+            leftim.src = "../img/fractal1hr.jpg";
+            rightim.src = "../img/fractal2hr.jpg";
 
-            messp.innerHTML = "Please select one of the items below. <br> Compensation for your choice will be <br> shown in the following screen."
+            leftdiv.style.backgroundColor = "#990000";
+            rightdiv.style.backgroudColor = "#003300";
+
+            statediv.style.display = "none";
+            instdiv.style.height = "100%";
+            instdiv.style.backgroundColor = "black";
+
+            messp.innerHTML = "Please select one of the items above. <br> Compensation for your choice will be <br> shown in the following screen."
         }
         else if (counter[8] < Trial_type1+Trial_type2+Trial_type3){
-            leftim.src = "../img/fractal3.png";
-            rightim.src = "../img/fractal4.jpg";
+            leftim.src = "../img/fractal3hr.jpg";
+            rightim.src = "../img/fractal4hr.jpg";
 
             leftdiv.style.backgroundColor = "#ff6600";
             rightdiv.style.backgroundColor = "#9900ff";
 
+            statediv.style.backgroundColor = "#003300";
             instdiv.style.backgroundColor = "#003300";
-            backdiv.style.backgroundColor = "#003300";
 
-            messp.innerHTML = "Please select one of the items below. <br> Compensation for your choice will be <br> shown in the following screen."
-            payim.src = "../img/fractal2.png";
-            payim.style.height = "80px";
-            payim.style.width = "120px";
-            payim.style.bottom = "";
-            payim.style.left = "750px";
-            payim.style.display = "block";
+            stateim.src = "../img/fractal2hr.jpg";
 
+            messp.innerHTML = "Please select one of the items above. <br> Compensation for your choice will be <br> revealed in the following screen."
         }
         else {
-            leftim.src = "../img/fractal5.jpg";
-            rightim.src = "../img/fractal6.jpg";
+            leftim.src = "../img/fractal5hr.jpg";
+            rightim.src = "../img/fractal6hr.jpg";
 
             leftdiv.style.backgroundColor = "#3333FF";
             rightdiv.style.backgroundColor = "#606060";
 
+            statediv.style.backgroundColor = "#990000";
             instdiv.style.backgroundColor = "#990000";
-            backdiv.style.backgroundColor = "#990000";
 
-            messp.innerHTML = "Please select one of the items below. <br>Compensation for your choice will be <br> shown in the following screen."
+            stateim.src = "../img/fractal1hr.jpg";
+
+            messp.innerHTML = "Please select one of the items above. <br>Compensation for your choice will be <br> revealed in the following screen."
         }
     }
     else {
-        backdiv.style.display = "block";
-        backdiv.style.backgroundColor = "black";
+        statediv.style.display = "none";
+        paydiv.style.display ="none";
+
         rightdiv.style.display = "none";
         leftdiv.style.display = "none";
+
         instdiv.style.display = "block";
-        messp.style.display = "block";
-        messp.style.fontSize = "3em";
-        messp.innerHTML = "Game is over. Thanks for playing!"
+        instdiv.style.height = "100%";
+        instdiv.style.backgroundColor = "black";
+
+        var total = 0;
+        for (var k=2;k<8;k++) {
+            total += payoutsch[k]*counter[k];
+        };
+
+        messp.style.bottom = "20%";
+        messp.style.fontSize = "4em";
+        var goodbyemssg = "Thank you for playing! ";
+        messp.innerHTML = goodbyemssg.concat("Today you earned $ ", total.toString());
     }
     var timing = new Date().getTime();
     active = true;
@@ -129,19 +157,31 @@ var main = function(){
         click_time = new Date().getTime();
 
         var id = $(this).attr("id");
-        document.getElementById(id).width = "100%";
-        document.getElementById(id).height = "100%";
-        document.getElementById(id).left = "0px";
-        document.getElementById(id).top = "0px";
-        document.getElementById(id).className = "";
+        document.getElementById(id).style.width = "100%";
+        document.getElementById(id).style.height = "100%";
+        document.getElementById(id).style.left = "0px";
+        document.getElementById(id).style.top = "0px";
 
         $(this).siblings().css("display","none");
-        $(this).css('width','100%');
-        $(this).css('height','100%');
-        $(this).animate({left:"0px",top:"0px"},0);
-        $(".fractal").css('margin-left','650px');
 
-        var payimg = document.getElementById("PayorState");
+        var pix = document.getElementsByClassName("fractal");
+        pix[0].style.width = "25%";
+        pix[0].style.height = "25%";
+        pix[0].style.left = "50%";
+        pix[0].style.top = "5%";
+
+        pix[1].style.width = "25%";
+        pix[1].style.height = "25%";
+        pix[1].style.left = "50%";
+        pix[1].style.top = "5%";
+
+        var paydiv = document.getElementById("outcomescreen");
+        var payimg = document.getElementById("Payout");
+        var statediv = document.getElementById("statescreen");
+
+        var selectioncolor = document.getElementById(id).style.backgroundColor;
+
+        statediv.style.display = "none";
 
         if (active) {
 
@@ -155,31 +195,40 @@ var main = function(){
             }
 
             else if (counter[8] < Trial_type1+Trial_type2) {
+                paydiv.style.display = "block";
+                paydiv.style.backgroundColor = selectioncolor;
+                paydiv.style.height  = "70%";
                 if (id==='left_A') {
                     counter[2]++;
-                    payimg.src = "../img/dollarbill.jpg";
+                    payimg.src = "../img/penny.jpg";
                 }
                 else {
                     counter[3]++;
                     payimg.src = "../img/quarter.jpg";
                 }
-                payimg.style.marginLeft = "500px";
-                payimg.style.marginBottom = "100px";
             }
             else if (counter[8] < Trial_type1+Trial_type2+Trial_type3) {
+                paydiv.style.display = "block";
+                paydiv.style.backgroundColor = selectioncolor;
                 if (id==='left_A') {
                     counter[4]++;
+                    payimg.src = "../img/quarter.jpg";
                 }
                 else {
                     counter[5]++;
+                    payimg.src = "../img/dollarbill.jpg";
                 }
             }
             else if (counter[8] < maxTrials) {
-                if (id==='left_A') {
+                paydiv.style.display = "block";
+                paydiv.style.backgroundColor = selectioncolor;
+                 if (id==='left_A') {
                     counter[6]++;
+                    payimg.src = "../img/fivedollarbill.jpg";
                 }
                 else {
                     counter[7]++;
+                    payimg.src = "../img/tendollarbill.jpg";
                 }
             }
             counter[8]++;
